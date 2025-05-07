f2c68eff2d7c9c75799e4b2bbf78f92be42f093d
madogkwon <a93081077a@gmail.com>
2025-05-07 17:24:43 +0900

    refactor : 댓글 내용 null 비허용

--- src/main/java/org/example/capstone/dto/RequestCommentDto.java
+++ src/main/java/org/example/capstone/dto/RequestCommentDto.java
@@ -1,11 +1,13 @@
 package org.example.capstone.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.example.capstone.domain.Post;
 
 @Getter @Setter
 public class RequestCommentDto {
+  @NotBlank(message = "내용은 필수입니다")
   private String content;
   private Long parentId;
 }
