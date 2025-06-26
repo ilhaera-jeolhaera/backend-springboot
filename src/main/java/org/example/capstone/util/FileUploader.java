206e2239ea263f3c56d2949bc9ce48cb9db25e1c
madogkwon <a93081077a@gmail.com>
2025-06-26 20:05:16 +0900

    feat: 로컬 업로드 방식에서 R2 업로드로 변경

--- src/main/java/org/example/capstone/util/FileUploader.java
+++ src/main/java/org/example/capstone/util/FileUploader.java
@@ -1,52 +1,80 @@
 package org.example.capstone.util;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.core.sync.RequestBody;
 
-import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
 public class FileUploader {
 
-  @Value("${file.upload-dir:app/tmp/uploads}")
-  private String uploadDir;
+  @Value("${cloudflare.r2.access-key}")
+  private String accessKey;
 
-  @Value("${server.base-url:https://port-0-backend-springboot-mbhk52lab25c23a5.sel4.cloudtype.app}")
-  private String baseUrl;
+  @Value("${cloudflare.r2.secret-key}")
+  private String secretKey;
 
-  public String upload(MultipartFile file) throws IOException {
-    File directory = new File(uploadDir);
-    if (!directory.isAbsolute()) {
-      directory = new File(System.getProperty("user.dir"), uploadDir);
-    }
+  @Value("${cloudflare.r2.endpoint}")
+  private String endpoint;
 
-    if (!directory.exists()) {
-      if (!directory.mkdirs()) {
-        throw new IOException("업로드 디렉토리 생성 실패: " + directory.getAbsolutePath());
-      }
-    }
+  @Value("${cloudflare.r2.bucket}")
+  private String bucketName;
 
-    String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
-    File destination = new File(directory, fileName);
-    file.transferTo(destination);
+  @Value("${cloudflare.r2.region}")
+  private String region;
 
-    return baseUrl + "/images/" + fileName;
+  private S3Client s3Client;
+
+  @PostConstruct
+  public void init() {
+    s3Client = S3Client.builder()
+            .endpointOverride(URI.create(endpoint))
+            .credentialsProvider(
+                    StaticCredentialsProvider.create(
+                            AwsBasicCredentials.create(accessKey, secretKey)
+                    )
+            )
+            .region(Region.of(region))
+            .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
+            .build();
   }
 
+  public String upload(MultipartFile file) throws IOException {
+    String fileName = "post/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
 
-  public void delete(String imageUrl) throws IOException {
+    PutObjectRequest request = PutObjectRequest.builder()
+            .bucket(bucketName)
+            .key(fileName)
+            .contentType(file.getContentType())
+            .build();
+
+    s3Client.putObject(request, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+    return endpoint + "/" + fileName;
+  }
+
+  public void delete(String imageUrl) {
     String fileName = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
-    File file = new File(uploadDir, fileName);
 
-    if (file.exists()) {
-      if (!file.delete()) {
-        throw new IOException("파일 삭제 실패 : " + file.getAbsolutePath());
-      }
-    }
+    DeleteObjectRequest request = DeleteObjectRequest.builder()
+            .bucket(bucketName)
+            .key(fileName)
+            .build();
+
+    s3Client.deleteObject(request);
   }
 }
