19e8439c351c12f0ecdc290a862ee1ff7bb13534
madogkwon <a93081077a@gmail.com>
2025-09-10 00:50:14 +0900

    refactor : 배포된 db 환경에 맞게 user 필드 수정

--- src/main/java/org/example/capstone/entity/User.java
+++ src/main/java/org/example/capstone/entity/User.java
@@ -4,13 +4,36 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @Table(name = "users")
 public class User {
-  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(name = "name")
+  @Column(name = "name", length = 20, nullable = false, unique = true)
   private String username;
+
+  @Column(name = "password", length = 60, nullable = false)
+  private String password;
+
+  @Column(name = "email", length = 50, nullable = false, unique = true)
+  private String email;
+
+  @Column(name = "refresh_token", length = 255)
+  private String refreshToken;
+
+  @Column(name = "created_at", updatable = false)
+  private LocalDateTime createdAt;
+
+  @PrePersist
+  protected void onCreate() {
+    this.createdAt = LocalDateTime.now();
+  }
 }
