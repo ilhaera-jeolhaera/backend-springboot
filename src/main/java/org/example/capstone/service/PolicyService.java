96af9415e937c9a2cfdf8218830dcc8861558437
madogkwon <a93081077a@gmail.com>
2025-09-10 00:25:28 +0900

    feat : sprvsn_inst_cd_nm 검색을 FULL TEXT SEARCH 에서 LIKE 검색으로 변경

--- src/main/java/org/example/capstone/service/PolicyService.java
+++ src/main/java/org/example/capstone/service/PolicyService.java
@@ -1,34 +1,33 @@
 package org.example.capstone.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.example.capstone.dto.PolicyDto;
 import org.example.capstone.entity.Policy;
 import org.example.capstone.repository.PolicyRepository;
-import org.springframework.data.domain.*;
+import org.example.capstone.specification.PolicySpecification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PolicyService {
   private final PolicyRepository policyRepository;
 
   public Page<PolicyDto> getAllPolicies(int page, int progress, Integer startAge, Integer endAge, String organ) {
-    try {
-      log.info("정책 조회 요청 - 페이지: {}, 진행상태: {}, 시작연령: {}, 종료연령: {}, 기관: {}", 
-               page, progress, startAge, endAge, organ);
-      
-      Pageable pageable = PageRequest.of(page, 8);
-      
-      Page<Policy> policyPage = policyRepository.searchPolicies(organ, startAge, endAge, progress, pageable);
-      log.info("조회된 정책 수: {}", policyPage.getTotalElements());
-      
-      return policyPage.map(PolicyDto::from);
-      
-    } catch (Exception e) {
-      log.error("정책 조회 중 오류 발생: {}", e.getMessage(), e);
-      throw e;
-    }
+
+    Pageable pageable = PageRequest.of(page, 8);
+
+    Specification<Policy> spec = Specification
+            .where(PolicySpecification.matchOrgan(organ))
+            .and(PolicySpecification.ageCheck(startAge, endAge))
+            .and(PolicySpecification.progressCheck(progress));
+
+    Page<Policy> policyPage = policyRepository.findAll(spec, pageable);
+
+    return policyPage.map(PolicyDto::from);
+
   }
 }
